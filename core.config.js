5161990edc45932d6c63c01d91deb58ee0ac3367
Pablo Zadro <pablozadro@gmail.com>
2022-10-29 15:28:53 -0300

    added manifesto

--- core.config.js
+++ core.config.js
@@ -4,5 +4,6 @@ module.exports = {
   assets: {
     brand: path.resolve(__dirname, 'src/assets'),
     favicon: path.resolve(__dirname, 'src'),
+    manifest: path.resolve(__dirname, 'src'),
   }
 }
