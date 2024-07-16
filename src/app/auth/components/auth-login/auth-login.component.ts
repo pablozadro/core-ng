4341fd0ffe25a1079d6652a1c507bdfcddd69a04
pablozadro <pablozadro@gmail.com>
2024-07-16 10:29:27 -0300

    WIP: login

--- src/app/auth/components/auth-login/auth-login.component.ts
+++ src/app/auth/components/auth-login/auth-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CoreApiService } from '@/core/services/core-api.service';
 
@@ -7,6 +8,7 @@ import { CoreApiService } from '@/core/services/core-api.service';
   selector: 'app-auth-login',
   standalone: true,
   imports: [
+    NgIf,
     ReactiveFormsModule
   ],
   templateUrl: './auth-login.component.html',
@@ -18,7 +20,7 @@ export class AuthLoginComponent {
   SHOW_HIDE_HIDE_TEXT = 'hide';
   SHOW_HIDE_PASSWORD_TYPE = 'password';
   SHOW_HIDE_TEXT_TYPE = 'text';
-  AUTH_LOGIN_URL = '/api/v1/auth/login';
+  AUTH_LOGIN_URL = 'http://localhost:9001/api/v1/auth/login';
 
   showHidePasswordType = this.SHOW_HIDE_PASSWORD_TYPE;
   showHidePasswordText = this.SHOW_HIDE_SHOW_TEXT;
@@ -54,9 +56,12 @@ export class AuthLoginComponent {
 
   onFormSubmit() {
     if(this.form.invalid) return;
-    this.coreApiService.post(
-      this.AUTH_LOGIN_URL, 
-      { email: this.email.value, password: this.password.value }
-    )
+    this.coreApiService
+      .post(
+        this.AUTH_LOGIN_URL, 
+        { email: this.email.value, password: this.password.value }
+      )
+      .subscribe(res => {
+      })
   }
 }
